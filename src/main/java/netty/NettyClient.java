7112fa3c186633e507800155439146411d19358c
jiangruliang <jiangruliang@yeah.net>
2019-08-20 16:48:37 +0800

    Netty建立客户端、服务端学习

--- src/main/java/netty/NettyClient.java
+++ src/main/java/netty/NettyClient.java
@@ -8,6 +8,7 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName NettyClient
@@ -18,21 +19,44 @@ import java.util.Date;
  */
 public class NettyClient {
     public static void main(String[] args) throws InterruptedException {
-        Bootstrap bootstrap=new Bootstrap();
-        NioEventLoopGroup group=new NioEventLoopGroup();
-        bootstrap.group(group)
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        bootstrap
+                //1.指定线程模型
+                .group(workerGroup)
+                //2.指定io类型为NIO
                 .channel(NioSocketChannel.class)
+                //3.处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel channel) {
                         channel.pipeline().addLast(new StringDecoder());
                     }
                 });
-        Channel channel=bootstrap.connect("127.0.0.1",8000).channel();
-        while (true){
-            channel.writeAndFlush(new Date()+"hello world!");
-            Thread.sleep(2000);
-        }
+        //建立连接
+        connect(bootstrap, "127.0.0.1", 1000, MAX_RETRY);
 
+
+
+    }
+
+    final private static int MAX_RETRY = 5;
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功！");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接");
+            } else {
+                //第几次重连
+                int order = MAX_RETRY - retry + 1;
+                //本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + "连接失败！,第" + order + "次重连....");
+                bootstrap.config().group().schedule(
+                        () -> connect(bootstrap, host, port, retry - 1),delay, TimeUnit.SECONDS);
+            }
+        });
     }
 }
--- src/main/java/netty/NettyServer.java
+++ src/main/java/netty/NettyServer.java
@@ -3,11 +3,14 @@ package netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * @ClassName NettyServer
@@ -18,18 +21,21 @@ import io.netty.handler.codec.string.StringDecoder;
  */
 public class NettyServer {
     public static void main(String[] args) {
-        ServerBootstrap serverBootstrap=new ServerBootstrap();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
         //接受新连接线程，负责创建新连接
-        NioEventLoopGroup boss=new NioEventLoopGroup();
+        NioEventLoopGroup boss = new NioEventLoopGroup();
         //负责读取数据的线程，主要用于读取数据以及业务逻辑处理
-        NioEventLoopGroup worker=new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
         serverBootstrap
-                .group(boss,worker)
+                //线程模型
+                .group(boss, worker)
+                //io模型
                 .channel(NioServerSocketChannel.class)
+                //连续数据读写处理
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) {
-                       ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new StringDecoder());
                         ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
                             @Override
                             protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) {
@@ -37,6 +43,24 @@ public class NettyServer {
                             }
                         });
                     }
-                }).bind(8000);
+                })
+                //TCP底层心跳机制，true为开启
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                //true表示关闭，false表示开启，通俗的说，如果要求高实时性，就关闭，如果需要减少网络交互，就开启
+                .childOption(ChannelOption.TCP_NODELAY,false)
+                //用于已完成三次握手请求队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+                .option(ChannelOption.SO_BACKLOG,1024);
+        bind(serverBootstrap, 1000);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
     }
 }
